ef7f06e18b3b9adc48bc1284502d9077961432d3
josellanos95 <jose.llanos95@gmail.com>
2024-10-21 17:37:26 -0300

    render deployment changes

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -19,4 +19,7 @@ export const AppDataSource = new DataSource({
   entities: [User, Credential, Order, Product, Category],
   subscribers: [],
   migrations: [],
+  ssl: {
+    rejectUnauthorized: false
+}
 });
